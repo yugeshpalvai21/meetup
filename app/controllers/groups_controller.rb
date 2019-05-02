17e658c967194acb158fe4770cef71c55097babd
yugeshreddypalvai <pyreddy517@hotmail.com>
2019-05-02 11:59:29 +0530

    Implements Group new and create actions and templates - adds validations to Group Model

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,9 +6,17 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:notice] =  "Group Created Successfully"
+      redirect_to group_path(@group)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -18,5 +26,11 @@ class GroupsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,10 @@
 class Group < ApplicationRecord
+	validates :name, presence: true
+	validates :name, length: {minimum: 3, maximum: 12}
+
+	before_save :to_lower
+
+	def to_lower
+		self.name = self.name.downcase
+	end
 end
