9d386f9d3ff9c943c129ea03e2e1acbc00225839
yugeshreddypalvai <pyreddy517@hotmail.com>
2019-05-02 16:22:09 +0530

    adds routes for import-data and process-data and adds actions to Home Controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def import_data
+  end
+
+  def process_data
+  	flash[:success] = "Data Processing Successfully"
+  	redirect_to root_path
+  end
 end
